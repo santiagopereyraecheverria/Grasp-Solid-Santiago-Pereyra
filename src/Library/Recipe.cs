b4149739bf1bb76f4ef1a9ef2eb1c91e494aa03a
santiagopereyraecheverria <141652797+santiagopereyraecheverria@users.noreply.github.com>
2024-04-15 17:03:59 -0300

    Update Recipe.cs

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -4,16 +4,20 @@
 // </copyright>
 //-------------------------------------------------------------------------
 
-using System;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID.Library
 {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        private List<Step> steps = new List<Step>();
 
         public Product FinalProduct { get; set; }
+
+        public List<Step> Steps // Agregamos una propiedad para acceder a los pasos
+        {
+            get { return steps; }
+        }
 
         public void AddStep(Step step)
         {
@@ -33,6 +37,10 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+
+            // Calcula e imprime el costo total de producción
+            double totalCost = ProductionCostCalculator.CalculateTotalProductionCost(this);
+            Console.WriteLine($"Costo total de producción: {totalCost}");
         }
     }
 }
